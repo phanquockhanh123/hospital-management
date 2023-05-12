a251e00079de1bea5911c115ec00381f676d1681
phanquockhanh123 <khanhphanquoc68@gmail.com>
2023-05-12 21:09:23 +0700

    handle banner

--- resources/views/user/banner.blade.php
+++ resources/views/user/banner.blade.php
@@ -2,29 +2,34 @@
     
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style="margin-top:-50px;">
         <div class="carousel-inner">
-            <div class="carousel-item active">
+            <div class="carousel-item ">
                 <img class="d-block w-100" width="100%"
-                    src="https://th.bing.com/th/id/R.bff0cf117e3635c8081f7f7d99964f29?rik=bRT614ium3EU7g&pid=ImgRaw&r=0"
+                    src="{{ asset('./imgBanners/slider1.png') }}"
                     height="500px" alt="First slide">
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100"
-                    src="https://www.webinventiv.com/images/services/hospital-management-system.png" height="500px"
+                    src="{{ asset('./imgBanners/slider2.png') }}" height="500px"
                     alt="Second slide">
             </div>
-            <div class="carousel-item">
+            <div class="carousel-item ">
                 <img class="d-block w-100"
-                    src="https://acsonnet.com/wp-content/uploads/2021/05/Hospital-Management-System.jpg" height="500px"
+                    src="{{ asset('./imgBanners/slider3.png') }}" height="500px"
                     alt="Third slide">
             </div>
-            <div class="carousel-item">
+            <div class="carousel-item active">
                 <img class="d-block w-100"
-                    src="https://th.bing.com/th/id/R.e2e5a0c6654364ec966a184312270e03?rik=uvBb3%2bpJvRv1ag&riu=http%3a%2f%2fonline.shsu.edu%2fdegrees%2fundergraduate%2fhealth-care-administration%2fmedia%2fHealth-Care-Administration_updated.jpg&ehk=DDksngocyKfADuWLrKY6jwkNvojKRQs1rIa7s8G%2boLM%3d&risl=&pid=ImgRaw&r=0" height="500px"
+                    src="{{ asset('./imgBanners/slider4.png') }}" height="500px"
                     alt="Second slide">
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100"
-                    src="https://businessfirstfamily.com/wp-content/uploads/2017/03/healthcare-management-adminstration-differences.jpg" height="500px"
+                    src="{{ asset('./imgBanners/slider5.png') }}" height="500px"
+                    alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100"
+                    src="{{ asset('./imgBanners/slider6.png') }}" height="500px"
                     alt="Second slide">
             </div>
         </div>
